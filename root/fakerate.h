464469a82c3ba2f756096865ad15b16f47689e1e
Markus Radziej <radziej@physik.rwth-aachen.de>
2014-05-06 13:36:23 +0200

    Added get_fakes_2d and closure_estimate_1d to header.

--- root/fakerate.h
+++ root/fakerate.h
@@ -3,8 +3,11 @@
 
 void tightlooseplots(int start = 1, int end = 5);
 void tight_loose_ratioplot();
+TH1D * get_fakes_1d(const char * hname);
+TH2D * get_fakes_2d(const char * hname);
 TH1D * get_subtracted_tight_loose_ratio(bool save=true, bool draw=true);
 TH1D * fake_estimate_1d(const char * sel, const char * hname);
 TH2D * fake_estimate_2d(const char * sel, const char * hname);
+TH1D * closure_estimate_1d(const char * sel, const char * hname);
 
 #endif
